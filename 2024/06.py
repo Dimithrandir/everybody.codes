2fa3167eab2b466ac1d23f82b724c9ca55d4ff29
Dimithrandir <24793925+Dimithrandir@users.noreply.github.com>
2025-11-04 19:16:26 +0100

    2024 06 solved

--- 2024/06.py
+++ 2024/06.py
@@ -1,29 +1,48 @@
-def solve_part_one(nails):
+# dfs with stack
+def find_all_paths(tree, node, path, all_paths):
+    path.append(node)
+    if node == '@':
+        all_paths.append(path.copy())
+    elif node in tree.keys():
+        for neighbor in tree[node]:
+            if neighbor not in ['ANT', 'BUG']:
+                find_all_paths(tree, neighbor, path, all_paths)
+    path.pop()
 
-    lowest = min(nails)
-    print(sum([x - lowest for x in nails]))
+
+def solve_part_one(tree, initials=False):
+    # get all paths from root to fruit
+    all_paths = []
+    find_all_paths(tree, 'RR', [], all_paths)
+    # group paths by length
+    lengths = {}
+    for path in all_paths:
+        str_path = ''.join([x[0] for x in path] if initials else path)
+        if len(path) not in lengths.keys():
+            lengths[len(path)] = [str_path]
+        else:
+            lengths[len(path)].append(str_path)
+    # print the shortest path with unique length
+    print(lengths[min(lengths.keys(), key=lambda l: len(lengths[l]))][0])
 
 
-def solve_part_two(nails):
+def solve_part_two(tree, initials=True):
 
-    solve_part_one(nails)
+    solve_part_one(tree, initials)
 
 
-def solve_part_three(nails):
+def solve_part_three(tree, initials=True):
 
-    min_strikes = nails[0] * 10
-    for nail in nails:
-        min_strikes = min(min_strikes, sum([abs(x - nail) for x in nails]))
-
-    print(min_strikes)
+    solve_part_one(tree, initials)
 
 
 if __name__ == '__main__':
 
     data = []
     for i in range(1, 4):
-        with open('04_p%d.txt' % i) as file:
-            data.append([int(x) for x in file.read().splitlines()])
+        with open('06_p%d.txt' % i) as file:
+            data.append({x.split(':')[0]: x.split(':')[1].split(',')
+                         for x in file.read().splitlines()})
 
     solve_part_one(data[0])
     solve_part_two(data[1])
